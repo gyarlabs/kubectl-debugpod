3b326fc26dd63aa0dba5b851e58ce665b10e2c07
Your Name <you@example.com>
2025-05-22 20:22:21 +0700

    add out with args

--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -2,15 +2,25 @@ package cluster
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
-func RunClusterCheck(args []string) error {
-	cmdArgs := append([]string{"analyze", "--output", "text"}, args...)
-	cmd := exec.Command("k8sgpt", cmdArgs...)
-	cmd.Stdout = nil
-	cmd.Stderr = nil
+func RunClusterCheck(args []string) {
+	baseArgs := []string{"analyze", "--output", "text"}
 
-	fmt.Println("Running k8sgpt:", cmd.String())
-	return cmd.Run()
+	// Append additional user-supplied args
+	baseArgs = append(baseArgs, args...)
+
+	fmt.Println("Running k8sgpt:", "k8sgpt", baseArgs)
+
+	cmd := exec.Command("k8sgpt", baseArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error running k8sgpt: %v\n", err)
+	}
 }
